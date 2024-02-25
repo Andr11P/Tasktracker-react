30e64506d864cc807a9ce0ceaa44914c42426853
Andrii Pohorielov <128001519+Andr11P@users.noreply.github.com>
2024-02-24 17:21:15 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,18 +22,18 @@ const App = () => {
 
   // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks')
-    const data = await res.json()
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
 
-    return data
+    return data;
   }
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
-    const data = await res.json()
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
 
-    return data
+    return data;
   }
 
   // Add Task
